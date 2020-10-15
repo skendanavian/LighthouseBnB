90b984a5ab761d9f8d1b1b839a9d72a6733c0b54
Soren Nissen <s.nissen.bass@gmail.com>
2020-10-15 20:13:38 +0000

    final commit before handing in

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -160,7 +160,6 @@ const addProperty = function(property) {
      owner_id, title, description,thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
     VALUES (
     $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`, newProperty).then(res => {
-    console.log(res.rows[0])
     return res.rows[0];
   })
 }
