d9850d621d5d0c954a9e7d89c6e5d7f86cf005e9
Soren Nissen <s.nissen.bass@gmail.com>
2020-10-15 01:59:26 +0000

    added pool to access database for getAllProperties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,18 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const {Pool} = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb',
+  port: 5432
+});
+
+
+
 /// Users
 
 /**
@@ -38,7 +50,7 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
+const addUser = function(user) {
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
@@ -67,13 +79,17 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  return pool.query(`
+  SELECT * FROM properties
+  LIMIT $1
+  `, [limit])
+    .then(res => {
+      return res.rows;
+    });
 }
 exports.getAllProperties = getAllProperties;
+
+
 
 
 /**
